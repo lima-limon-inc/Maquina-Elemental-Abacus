09dfc0d8e1905a1df3d53a494d23afa1479da665
Fabri <moboficial00@gmail.com>
2022-10-19 01:20:59 -0300

    Progreso hecho, estructura de las primitivas hechas

--- maquina_elemental.py
+++ maquina_elemental.py
@@ -1,13 +1,52 @@
 
 
 class Maquina_Elemental:
-    def __init__(self,acumulador, tablero, celdaInicial):
-        self.acumulador = acumulador #Acumulador de la maquina
-        self.tablero = tablero #Diccionario de celdas
+    def __init__(self, celdaInicial):
+        self.acumulador = 0 #Acumulador de la maquina
+        self.tablero = {} #Diccionario de celdas
         self.celdaActual = celdaInicial
 
+    def obtenerValorcelda(self, celda): #Esta funcion devuelve el valor almacenado en la celda. No es parte de las primitivas de la maquina Abacus, sino para la facilitacion del codigo
+        return self.tablero[celda]
+    
+    def cargaInmediata(self, valor): # 0 guarda el valor pasado directamente en el acumulador
+        self.acumulador = valor
+
+    def carga(self, celda): # 1 Carga el valor de la celda en el acumulador
+        self.acumulador = self.obtenerValorcelda(celda)
+
+    def almacenar(self, celda): # 2 Guarda el contenido del acumulador en la celda pasada
+        self.tablero[celda] = self.acumulador 
+
+    def suma(self, celda): # 3 Suma el contenido de la celda al acumulador y lo deja en el acumulador
+        self.acumulador += self.obtenerValorcelda(celda)
+
+    def notAbacus(self): # 4 Le aplica el not al acumulador TODO: Como funciona
+        pass
+
+    def difIgual(self, celda): # 7 Si el contenido de la celda es igual a 0, entonces vamos a esa celda
+        if self.obtenerValorcelda(celda) == 0:
+            self.celdaActual = celda
+
+    def difMenor(self, celda): # 8 Si el contenido de la celda es menor a 0, entonces vamos a esa celda
+        if self.obtenerValorcelda(celda) < 0:
+            self.celdaActual = celda
+
+    def difMayor(self, celda): # 9 Si el contenido de la celda es mayor a 0, entonces vamos a esa celda
+        if self.obtenerValorcelda(celda) > 0:
+            self.celdaActual = celda
+
 class Celda:
-    def __init__(self,valor):
+    def __init__(self,valor, comentario):
         self.valor = valor
+        self.comentario = comentario
 
+def main():
+    maqEle = Maquina_Elemental(300)
+    print(maqEle.tablero)
+    print(maqEle.acumulador)
+    print(maqEle.celdaActual)
+    maqEle.almacenar(500)
+    print(maqEle.tablero)
 
+main()
