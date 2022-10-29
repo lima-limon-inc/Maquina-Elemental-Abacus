0cb7145c1ee0aa1e853bccc46acacb7f1185b085
Fabri <moboficial00@gmail.com>
2022-10-29 17:30:29 -0300

    Rediseño en teoria listo

--- maquina_elemental.py
+++ maquina_elemental.py
@@ -2,12 +2,29 @@
 
 class Maquina_Elemental:
     def __init__(self, celdaInicial):
-        self.acumulador = 0 #Acumulador de la maquina
+        self.acumulador = "0" #Acumulador de la maquina
         self.tablero = {} #Diccionario de celdas
-        self.celdaActual = int(celdaInicial, 16) #celdaInicial es un string que representa un numero en base 16
+        '''
+        Cada celda es un numero binario en formato hexadecimal de 3 bytes, que tiene guardada una celda de 4 bytes (1 byte para la instruccion y los otros 3 bytes para la celda a la que se le aplica). Nosotros guardamos las celdas como strings que representan numeros en base 16
+        {
+        300: 1500
+        301: 2300
+        }
+        '''
+        self.celdaActual = celdaInicial #String que representa un numero en base 16
+
+    def mostrarAcumulador(self):
+        return self.acumulador
+
+    def pasarABinario(self, valor): #TODO: Borrar
+        return bin(valor)#.split("b",1)[1]#[2:]
 
     def obtenerValorcelda(self, celda): #Esta funcion devuelve el valor almacenado en la celda. No es parte de las primitivas de la maquina Abacus, sino para la facilitacion del codigo
         return self.tablero[celda]
+
+    def siguienteCelda(self):
+        hex(int (self.celdaActual, 16) + 1).split("x",1)[1] #Pasa el numero a decimal, le suma 1, lo pasa a hexadecimal y le saca el "0x" de python del principio
+
     
     def cargaInmediata(self, valor): # 0 guarda el valor pasado directamente en el acumulador
         self.acumulador = valor
@@ -21,8 +38,8 @@ class Maquina_Elemental:
     def suma(self, celda): # 3 Suma el contenido de la celda al acumulador y lo deja en el acumulador
         self.acumulador += self.obtenerValorcelda(celda)
 
-    def notAbacus(self): # 4 Le aplica el not al acumulador TODO: Como funciona
-        pass
+    def notAbacus(self): # 4 Le aplica el not al acumulador
+        self.acumulador = ~self.acumulador
 
     def difIgual(self, celda): # 7 Si el contenido de la celda es igual a 0, entonces vamos a esa celda
         if self.obtenerValorcelda(celda) == 0:
@@ -45,8 +62,14 @@ def main():
     maqEle = Maquina_Elemental("300")
     print(maqEle.tablero)
     print(maqEle.acumulador)
+    print(maqEle.mostrarAcumulador())
     print(maqEle.celdaActual)
     maqEle.almacenar(500)
-    print(maqEle.tablero)
-
+    print(maqEle.pasarABinario(300))
+    maqEle.cargaInmediata(300)
+    print(maqEle.acumulador)
+    print(maqEle.mostrarAcumulador())
+    maqEle.notAbacus()
+    print(maqEle.acumulador)
+    print(maqEle.mostrarAcumulador())
 main()
